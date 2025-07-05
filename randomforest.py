527cbedfa01c271caf877e91e504996f7dfaf8f2
NarraRanjith <153351320+NarraRanjith@users.noreply.github.com>
2025-07-05 21:38:35 +0530

    Create randomforest.py

---
+++ randomforest.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder # Import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+data=pd.read_csv("cibil_score_dataset_final.csv")
+print("no. of Null values")
+print(data.isnull().sum())
+columns_to_drop = ['Name','Occupation','Bank']
+data = data.drop(columns=columns_to_drop, axis = 1)
+data.drop_duplicates()
+X=data.drop(['Score_Category'],axis=1)
+y=data['Score_Category']
+
+# Encode the target variable 'y' before splitting
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+knn=RandomForestClassifier(n_estimators=100,     max_depth=None,      random_state=42)
+knn.fit(X_train,y_train)
+y_pred=knn.predict(X_test)
+accuracy=accuracy_score(y_test,y_pred)
+cm=confusion_matrix(y_test,y_pred)
+cr=classification_report(y_test,y_pred)
+print(f"accuracy:{accuracy}")
+print("confusion matrix")
+print(cm)
+print("classification Report")
+print(cr)
