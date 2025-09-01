6c3c2afe32b3577edbe24c510a58672267eb2678
NarraRanjith <narraranjith22@gmail.com>
2025-09-01 19:08:24 +0530

    data_validation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-"""from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+"""from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 """
@@ -17,7 +17,6 @@ except Exception as e:
         raise e
 
 
-"""
 
 STAGE_NAME = "Data Validation stage"
 try:
@@ -30,6 +29,7 @@ except Exception as e:
         raise e
 
 
+"""
 
 STAGE_NAME = "Data Transformation stage"
 try:
